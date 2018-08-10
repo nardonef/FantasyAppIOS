67e305a18502f377679533e54b6753b5a7bd195b
Frank Nardone <fnardone@albany.edu>
2018-08-09 22:02:21 -0400

    Initial Commit

---
+++ Fantasy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fantasy
+//
+//  Created by Frank Nardone on 8/9/18.
+//  Copyright © 2018 FrankNardone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
